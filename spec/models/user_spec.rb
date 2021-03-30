fc05de28c7fb8c7d46993337201173a2e558365e
yukiukisato <yukiukisato@yahoo.co.jp>
2021-03-30 21:52:16 +0900

    ユーザー管理機能実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+  end
+end
