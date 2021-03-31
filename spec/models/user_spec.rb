c36c7b5c4aa0b4e1ab31f03c6ce0bd44297c14d0
yukiukisato <yukiukisato@yahoo.co.jp>
2021-03-31 10:34:33 +0900

    テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,8 +19,33 @@ RSpec.describe User, type: :model do
     it 'passwordが空では登録できない' do
       @user.password = ''
       @user.valid?
-      binding.pry
       expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+    it '姓が空では登録できない' do
+      @user.last_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name can't be blank"
+    end
+    it '名が空では登録できない' do
+      @user.first_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name can't be blank"
+    end
+    it '姓(カナ)が空では登録できない' do
+      @user.last_name_katakana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Last name katakana can't be blank"
+    end
+    it '名(カナ)が空では登録できない' do
+      @user.first_name_katakana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name katakana can't be blank"
+    end
+    it '生年月日が空では登録できない' do
+      @user.birthday = ''
+      @user.valid?
+      binding.pry
+      expect(@user.errors.full_messages).to include "Birthday can't be blank"
     end
   end
 end
