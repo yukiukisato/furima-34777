cceb1ed05011bf1d770b6f7b815bfdc7fba0d425
yukiukisato <yukiukisato@yahoo.co.jp>
2021-04-01 20:31:02 +0900

    バリデーション設定、商品ボタンからトップページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,10 @@
 class ItemsController < ApplicationController
   def  index
     
-    
+  end
+
+  def new
+    @item = Item.new
   end
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :version
+    validates :price
+    validates :image
+  end
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :shipping_fee_burden_id, numericality: { other_than: 1 }
+  validates :delivery_area_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
+  
   belongs_to :user
   has_one :histry
   has_one_attached :image
