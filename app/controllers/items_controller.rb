6652ea3d6eed0447476c056c298419dea606befe
yukiukisato <yukiukisato@yahoo.co.jp>
2021-04-02 16:38:11 +0900

    出品情報保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+ private
+
+  def item_params
+    params.require(:item).permit( :name, :version, :category_id, :status_id, :shipping_fee_burden_id, :delivery_area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+end
--- app/models/shipping_fee_burden.rb
+++ app/models/shipping_fee_burden.rb
@@ -1,1 +1,11 @@
-class Shipping_fee_burden < ActiveHash::Base
+class ShippingFeeBurden < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '着払い（購入者負担）' },
+    { id: 3, name: '送料込み（出品者負担）' }
+
+  ]
+ 
+   include ActiveHash::Associations
+   has_many :articles
+end
